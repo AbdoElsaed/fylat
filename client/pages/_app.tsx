27ad3a43d9a3004701fdbbd1fb8d36907fdf7c54
Abdulrhman Elsaed <abdoelsaed114@gmail.com>
2022-11-30 20:09:52 +0200

    client env vars

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,9 +2,8 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
 import { io } from "socket.io-client";
-import { StringDecoder } from "string_decoder";
 
-const ENDPOINT = process.env.SERVER_ENDPOINT as string;
+const ENDPOINT = process.env.NEXT_PUBLIC_SERVER_ENDPOINT as string;
 const socket = io(ENDPOINT);
 
 function MyApp({ Component, pageProps }: AppProps) {
